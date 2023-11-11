e2ff24a63f528ac06e9c40b54733de8cdf5550d3
Geert Bosch <geert@mongodb.com>
2023-11-11 22:49:24 +0000

    Add documentation
    Prompt: Document parsePiecePlacement and parseFEN

--- fen.h
+++ fen.h
@@ -3,5 +3,18 @@
 std::string toString(const ChessBoard& board);
 std::ostream& operator<<(std::ostream& os, const ChessBoard& board);
 
+/**
+ * @brief Parses the piece placement string of a FEN notation and returns a ChessBoard object.
+ *
+ * @param piecePlacement The piece placement string of a FEN notation.
+ * @return ChessBoard The ChessBoard object representing the parsed FEN notation.
+ */
 ChessBoard parsePiecePlacement(const std::string& piecePlacement);
+
+/**
+ * Parses a FEN string and returns the corresponding ChessPosition object.
+ *
+ * @param fen The FEN string to parse.
+ * @return The ChessPosition object corresponding to the given FEN string.
+ */
 ChessPosition parseFEN(const std::string& fen);
