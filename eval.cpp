6e06106634234bef9378e27818d045499a71c74b
Geert Bosch <geert@mongodb.com>
2023-12-26 16:53:45 +0000

    Add code to incrementally update a position's Hash

--- eval.cpp
+++ eval.cpp
@@ -100,6 +100,58 @@ public:
         toggle(piece, to);
     }
 
+    // Does not cancel out castling rights or en passant targets.
+    // Assumes that passed in board is the same as the board used to construct this hash.
+    void applyMove(const Board& board, Move mv) {
+        auto piece = board[mv.from];
+        auto target = board[mv.to];
+        move(piece, mv.from.index(), mv.to.index());
+        switch (mv.kind) {
+        case MoveKind::QUIET_MOVE: break;
+        case MoveKind::DOUBLE_PAWN_PUSH: toggle(ExtraVectors(mv.to.file() + EN_PASSANT_A)); break;
+        case MoveKind::KING_CASTLE:  // Assume the move has the king move, so adjust the rook here.
+            move(addColor(PieceType::ROOK, color(piece)),
+                 (color(piece) == Color::WHITE ? Position::whiteKingSideRook
+                                               : Position::blackKingSideRook)
+                     .index(),
+                 (color(piece) == Color::WHITE ? Position::whiteRookCastledKingSide
+                                               : Position::blackRookCastledKingSide)
+                     .index());
+            break;
+        case MoveKind::QUEEN_CASTLE:  // Assume the move has the king move, so adjust the rook here.
+            move(addColor(PieceType::ROOK, color(piece)),
+                 (color(piece) == Color::WHITE ? Position::whiteQueenSideRook
+                                               : Position::blackQueenSideRook)
+                     .index(),
+                 (color(piece) == Color::WHITE ? Position::whiteRookCastledQueenSide
+                                               : Position::blackRookCastledQueenSide)
+                     .index());
+            break;
+
+        case MoveKind::CAPTURE: toggle(target, mv.to.index()); break;
+        case MoveKind::EN_PASSANT:
+            // Depending of the color of our piece, the captured pawn is either above or below the
+            // destination square.
+            toggle(target, mv.to.index() + (color(piece) == Color::WHITE ? -kNumFiles : kNumFiles));
+            break;
+        case MoveKind::KNIGHT_PROMOTION:
+        case MoveKind::BISHOP_PROMOTION:
+        case MoveKind::ROOK_PROMOTION:
+        case MoveKind::QUEEN_PROMOTION:
+            toggle(piece, mv.to.index());  // Remove the pawn, add the promoted piece
+            toggle(addColor(promotionType(mv.kind), color(piece)), mv.to.index());
+            break;
+        case MoveKind::KNIGHT_PROMOTION_CAPTURE:
+        case MoveKind::BISHOP_PROMOTION_CAPTURE:
+        case MoveKind::ROOK_PROMOTION_CAPTURE:
+        case MoveKind::QUEEN_PROMOTION_CAPTURE:
+            toggle(target, mv.to.index());  // Remove the captured piece
+            toggle(piece, mv.to.index());   // Remove the pawn, add the promoted piece
+            toggle(addColor(promotionType(mv.kind), color(piece)), mv.to.index());
+            break;
+        }
+    }
+
     // Use toggle to add/remove a piece or non piece/location vector.
     void toggle(Piece piece, int location) { toggle(index(piece) * kNumSquares + location); }
     void toggle(int vector) { hash ^= hashVectors[vector]; }
