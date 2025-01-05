f7a922f820e54aaea5ba512e6fbb4e1839f74632
Geert Bosch <grbnyc@gmail.com>
2025-01-05 17:52:33 -0500

    Tweak transpositionTableEntries and add comment

--- options.h
+++ options.h
@@ -6,8 +6,8 @@ namespace options {
 
 constexpr int defaultDepth = 5;
 constexpr int quiescenceDepth = 4;
-constexpr size_t transpositionTableEntries = 1ull << 20;
-constexpr size_t stopCheckIterations = 4096;  // Power of two is most efficient.
+constexpr size_t transpositionTableEntries = 1ull << 15;  // Zero means not enabled
+constexpr size_t stopCheckIterations = 4096;              // Power of two is most efficient.
 
 constexpr bool transpositionTableDebug = false;
 
