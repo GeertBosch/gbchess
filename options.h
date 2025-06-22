4bf2142cfda4bbca60844f08f242cf70022a998a
Geert Bosch <grbnyc@gmail.com>
2025-06-22 15:40:33 -0400

    Move enabling caching for perft to options

--- options.h
+++ options.h
@@ -6,23 +6,22 @@
 namespace options {
 
 /**
- * Below options allow disabling various search optimizations.
+ * Below options allow disabling or tuning various search optimizations.
  */
-constexpr int defaultDepth = 12;
-constexpr int quiescenceDepth = 9;
-constexpr bool iterativeDeepening = true;                 // Aspiration windows need this
-constexpr bool lateMoveReductions = true;                 // Reduce moves at the end of the search
-constexpr bool historyStore = true;                       // Use beta cutoffs for move ordering
-constexpr std::array<int, 2> aspirationWindows{30, 125};  // Given in centipawns, {} disables
-constexpr int aspirationWindowMinDepth = 2;               // Minimum depth to use aspiration windows
-constexpr int promotionMinDepthLeft = 7;                  // Minimum depth left for promos in QS
-constexpr int currmoveMinDepthLeft = 6;                   // Min depth left for currmove progress
-constexpr size_t transpositionTableEntries = 1ull << 18;  // Zero means not enabled
-constexpr int defaultMoveTime = 8'000;                    // Max time for a move in milliseconds
-constexpr bool hash128 = true;                            // Use a 128-bit hash for chess positions
-
-constexpr bool incrementalEvaluation = true;  // During quiescence, compute evaluation using deltas
-
+constexpr int defaultDepth = 11;                            // Search depth in half-moves (plies)
+constexpr int quiescenceDepth = 9;                          // Depth of the quiescence search (QS)
+constexpr bool iterativeDeepening = true;                   // Aspiration windows need this
+constexpr bool lateMoveReductions = true;                   // Reduce moves at the end of the search
+constexpr bool historyStore = true;                         // Use beta cutoffs for move ordering
+constexpr std::array<int, 2> aspirationWindows{30, 125};    // Given in centipawns, {} disables
+constexpr int aspirationWindowMinDepth = 2;                 // Minimum depth for aspiration windows
+constexpr int promotionMinDepthLeft = quiescenceDepth - 2;  // Minimum depth left for promos in QS
+constexpr int currmoveMinDepthLeft = 6;                     // Min depth left for currmove progress
+constexpr size_t transpositionTableEntries = 1ull << 17;    // Zero means not enabled
+constexpr int defaultMoveTime = 8'000;                      // Max time for a move in milliseconds
+constexpr bool hash128 = true;                              // Use a 128-bit hash for  positions
+constexpr bool cachePerft = hash128;                        // Allow caching perft results
+constexpr bool incrementalEvaluation = true;                // Compute QS evaluation using deltas
 constexpr bool transpositionTableDebug = false;
 constexpr bool alphaBetaDebug = false;
 
