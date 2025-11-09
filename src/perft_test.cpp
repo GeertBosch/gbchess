b20c68b7cf2bd86f63d40590091338a08b6a4bdb
Geert Bosch <grbnyc@gmail.com>
2025-11-09 00:14:11 -0500

    Clean up known position testing

--- src/perft_test.cpp
+++ src/perft_test.cpp
@@ -26,36 +26,22 @@ int startingPositionDepth = kDebug ? 4 : 6;  // 197K vs 119M nodes
 
 using DepthAndCount = std::pair<int, NodeCount>;
 
-struct KnownPos {
+struct Known {
     std::string_view fen;
     int depth;
     NodeCount count;
-    constexpr KnownPos(int depth, NodeCount count)
+    constexpr Known(int depth, NodeCount count)
         : fen(fen::initialPosition), depth(depth), count(count) {}
-    constexpr KnownPos(std::string_view fen, DepthAndCount depthAndCount)
+    constexpr Known(std::string_view fen, int depth, NodeCount count)
+        : fen(fen), depth(depth), count(count) {}
+    constexpr Known(std::string_view fen, DepthAndCount depthAndCount)
         : fen(fen), depth(depthAndCount.first), count(depthAndCount.second) {}
 };
-
-constexpr KnownPos twoKings = {"4k3/8/8/8/8/8/8/4K3 w - - 0 1",
-                               kDebug ? DepthAndCount{8, 4'656'573}
-                                      : DepthAndCount{12, 10'109'930'485}};
-constexpr KnownPos maxMoves = {"R6R/3Q4/1Q4Q1/4Q3/2Q4Q/Q4Q2/pp1Q4/kBNN1KB1 w - - 0 1",
-                               kDebug ? DepthAndCount{3, 19'073} : DepthAndCount{5, 13'853'661}};
-constexpr KnownPos twoKingsRook = {"4k3/8/8/8/8/8/8/4Kr2 w - - 0 1",
-                                   kDebug ? DepthAndCount{5, 21'570} : DepthAndCount{7, 2'362'837}};
-constexpr KnownPos twoKingsOnly = {"4k3/8/8/8/8/8/8/4K3 w - - 0 1",
-                                   kDebug ? DepthAndCount{7, 375'049}
-                                          : DepthAndCount{10, 122'753'895}};
 
 /**
  * The following are well known perft positions, see https://www.chessprogramming.org/Perft_Results
  */
 void testKnownPositions() {
-    struct Known {
-        std::string fen;
-        int depth;
-        NodeCount count;
-    };
     Known kiwipete = {
         "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4, 4085603};
     Known position3 = {"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 5, 674624};
@@ -67,23 +53,31 @@ void testKnownPositions() {
     Known position6 = {
         "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 4, 3894594};
 
+    Known twoKings = {"8/8/3k4/8/8/8/8/2K5 w - - 4 3",
+                      kDebug ? DepthAndCount{8, 4'656'573} : DepthAndCount{12, 10'109'930'485}};
+    Known maxMoves = {"R6R/3Q4/1Q4Q1/4Q3/2Q4Q/Q4Q2/pp1Q4/kBNN1KB1 w - - 0 1",
+                      kDebug ? DepthAndCount{3, 19'073} : DepthAndCount{5, 13'853'661}};
+    Known twoKingsRook = {"4k3/8/8/8/8/8/8/4Kr2 w - - 0 1",
+                          kDebug ? DepthAndCount{5, 21'570} : DepthAndCount{7, 2'362'837}};
+    Known twoKingsOnly = {"4k3/8/8/8/8/8/8/4K3 w - - 0 1",
+                          kDebug ? DepthAndCount{7, 375'049} : DepthAndCount{10, 122'753'895}};
+
     std::vector<Known> knownPositions = {
-        kiwipete, position3, position4, position4m, position5, position6};
+        kiwipete, position3, position4, position4m, position5, position6, maxMoves};
 
     if (options::cachePerft) {
-        knownPositions.emplace_back(
-            Known{"4k3/8/8/8/8/8/8/4Kr2 w - - 0 1", twoKingsRook.depth, twoKingsRook.count});
-        knownPositions.emplace_back(
-            Known{"4k3/8/8/8/8/8/8/4K3 w - - 0 1", twoKingsOnly.depth, twoKingsOnly.count});
+        knownPositions.emplace_back(twoKings);
+        knownPositions.emplace_back(twoKingsRook);
+        knownPositions.emplace_back(twoKingsOnly);
     }
 
     for (const auto& known : knownPositions) {
-        Position position = fen::parsePosition(known.fen);
+        Position position = fen::parsePosition(std::string(known.fen));
         NodeCount count = perft(position, known.depth);
         if (count != known.count)
             error("Error: Expected " + to_string(known.count) + " nodes for position '" +
-                  known.fen + "' at depth " + std::to_string(known.depth) + ", but got " +
-                  to_string(count));
+                  std::string(known.fen) + "' at depth " + std::to_string(known.depth) +
+                  ", but got " + to_string(count));
 
         std::cout << "Passed: " << known.fen << " at depth " << known.depth
                   << ", count: " << to_string(count) << std::endl;
@@ -96,19 +90,19 @@ void testKnownPositions() {
  */
 void testStartingPosition(int depth) {
     assert(depth > 0 && depth <= 15);
-    std::vector<KnownPos> knownPositions = {{1, "20"_u128},
-                                            {2, "400"_u128},
-                                            {3, "8'902"_u128},
-                                            {4, "197'281"_u128},
-                                            {5, "4'865'609"_u128},
-                                            {6, "119'060'324"_u128},
-                                            {7, "3'195'901'860"_u128},
-                                            {8, "84'998'978'956"_u128},
-                                            {9, "2'439'530'234'167"_u128},
-                                            {10, "69'352'859'712'417"_u128},
-                                            {11, "2'097'651'003'696'806"_u128},
-                                            {12, "62'854'969'236'701'747"_u128},
-                                            {13, "1'981'066'775'000'396'239"_u128}};
+    std::vector<Known> knownPositions = {{1, "20"_u128},
+                                         {2, "400"_u128},
+                                         {3, "8'902"_u128},
+                                         {4, "197'281"_u128},
+                                         {5, "4'865'609"_u128},
+                                         {6, "119'060'324"_u128},
+                                         {7, "3'195'901'860"_u128},
+                                         {8, "84'998'978'956"_u128},
+                                         {9, "2'439'530'234'167"_u128},
+                                         {10, "69'352'859'712'417"_u128},
+                                         {11, "2'097'651'003'696'806"_u128},
+                                         {12, "62'854'969'236'701'747"_u128},
+                                         {13, "1'981'066'775'000'396'239"_u128}};
     if constexpr (sizeof(NodeCount) == sizeof(uint128_t)) {
         knownPositions.emplace_back(14, NodeCount("61'885'021'521'585'529'237"_u128));
         knownPositions.emplace_back(15, NodeCount("2'015'099'950'053'364'471'960"_u128));
@@ -123,27 +117,6 @@ void testStartingPosition(int depth) {
               << "\n";
 }
 
-void testTwoKings() {
-    if (!options::cachePerft) return;  // This test is too slow without caching
-    std::string fen = "8/8/3k4/8/8/8/8/2K5 w - - 4 3";
-    Position position = fen::parsePosition(fen);
-    NodeCount count = perft(position, twoKings.depth);
-    if (count != twoKings.count)
-        error("Expected " + to_string(twoKings.count) + " nodes for position '" + fen +
-              "' at depth " + std::to_string(twoKings.depth) + ", but got " + to_string(count));
-}
-
-void testMaxMoves() {
-    std::string fen = "R6R/3Q4/1Q4Q1/4Q3/2Q4Q/Q4Q2/pp1Q4/kBNN1KB1 w - - 0 1";
-    Position position = fen::parsePosition(fen);
-    NodeCount moves = perft(position, 1);
-    assert(moves == 218);  // Position with maximum number of legal moves
-    NodeCount count = perft(position, maxMoves.depth);
-    if (count != maxMoves.count)
-        error("Expected " + to_string(maxMoves.count) + " nodes for position '" + fen +
-              "' at depth " + std::to_string(maxMoves.depth) + ", but got " + to_string(count));
-}
-
 int main(int argc, char** argv) {
     if (argc == 2) startingPositionDepth = std::stoi(argv[1]);  // optional depth argument
 
@@ -152,8 +125,6 @@ int main(int argc, char** argv) {
 
     testStartingPosition(startingPositionDepth);
     testKnownPositions();
-    testTwoKings();
-    testMaxMoves();
     std::cout << "All perft tests passed!\n";
     return 0;
 }
