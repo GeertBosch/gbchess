d80361ddaaba987d7419413e042ef1df4b0d0bc6
Geert Bosch <grbnyc@gmail.com>
2025-11-15 13:42:01 -0500

    Clarify comment

--- src/random.h
+++ src/random.h
@@ -2,7 +2,7 @@
 #include <cstdint>
 /**
  * The xorshift PRNG provides a balance between quality and performance. Poor quality would lead to
- * longer search times, or in the worst case an unsuccessful search.
+ * longer search times for magic generation, or in the worst case an unsuccessful search.
  * See Marsaglia, George. (2003). Xorshift RNGs. Journal of Statistical Software.
  * 08. 10.18637/jss.v008.i14. https://www.researchgate.net/publication/5142825_Xorshift_RNGs
  */
