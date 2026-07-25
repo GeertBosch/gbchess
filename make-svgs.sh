35d2cf01bf010fb1d71f480f87141c58eacc7955
Geert Bosch <grbnyc@gmail.com>
2026-07-25 12:29:14 -0400

    Disable git grep colorization in make-svgs.sh
    color.ui=always in git config caused ANSI escape codes to leak into
    the parsed FEN/path strings, breaking SVG regeneration.

--- make-svgs.sh
+++ make-svgs.sh
@@ -3,7 +3,7 @@
 # chessboards for them. A FEN has 6 space-separated fields; we match just on the piece-placement
 # pattern (8 ranks separated by /).
 
-git grep -woE '[rnbqkpRNBQKP1-8]+(_[rnbqkpRNBQKP1-8]+){7}' |
+git grep --no-color -woE '[rnbqkpRNBQKP1-8]+(_[rnbqkpRNBQKP1-8]+){7}' |
 while IFS=: read md svg ; do
     out="$(dirname $md)/$svg.svg"
     tmp=$(mktemp)
